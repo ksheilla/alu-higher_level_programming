0f9409876a6ccd1ab4d9f9597439c700b182f5ee
ksheilla <s.ruvugabig@alustudent.com>
2024-11-01 16:51:05 +0200

    Task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,13 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             count += 1
-        except(IndexError):
+        except IndexError:
             break
-    print("")
+    print()
     return count
+
+
+if __name__ == "__main__":
+    print(safe_print_list([1, 2, 3, 4], 4))
+    print(safe_print_list([1, 2], 2))
+    print(safe_print_list([1, 2, 3, 4], 4))
