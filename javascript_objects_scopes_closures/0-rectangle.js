d1cd4148640bf71f0a0c4a53b0233e6c294b953a
ksheilla <s.ruvugabig@alustudent.com>
2025-01-27 01:30:22 +0200

    task 0

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./0-rectangle');
+class Rectangle {
+  // Empty class that defines a rectangle
+}
 
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.constructor);
+module.exports = Rectangle;
